297b35de37ae5e6d6dcdd23b903915e6c386f1be
bsnpbsi <kevin-x.li@aia.com>
2020-04-13 10:38:03 +0800

    Add allowFileAccessFromFileURLs

--- packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
+++ packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
@@ -51,7 +51,9 @@ public class FlutterWebView implements PlatformView, MethodCallHandler {
     // Allow local storage.
     webView.getSettings().setDomStorageEnabled(true);
     webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-
+    //add 
+    webView.getSettings().setAllowFileAccessFromFileURLs(true); 
+    
     methodChannel = new MethodChannel(messenger, "plugins.flutter.io/webview_" + id);
     methodChannel.setMethodCallHandler(this);
 
