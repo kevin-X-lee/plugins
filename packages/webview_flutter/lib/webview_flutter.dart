56a1f4c5913563ab1cff2aa170dba39605daf3a6
bsnpbsi <kevin-x.li@aia.com>
2020-05-06 16:31:09 +0800

    Revert "[webview_flutter] Add onReceivedError callback  (#2639)"
    This reverts commit 8819b219c5ca83a000ae482b9a51b7f1f421845b.
    
    # Conflicts:
    #	packages/webview_flutter/CHANGELOG.md
    #	packages/webview_flutter/pubspec.yaml

--- packages/webview_flutter/lib/webview_flutter.dart
+++ packages/webview_flutter/lib/webview_flutter.dart
@@ -79,9 +79,6 @@ typedef void PageStartedCallback(String url);
 /// Signature for when a [WebView] has finished loading a page.
 typedef void PageFinishedCallback(String url);
 
-/// Signature for when a [WebView] has failed to load a resource.
-typedef void WebResourceErrorCallback(WebResourceError error);
-
 /// Specifies possible restrictions on automatic media playback.
 ///
 /// This is typically used in [WebView.initialMediaPlaybackPolicy].
@@ -150,7 +147,6 @@ class WebView extends StatefulWidget {
     this.gestureRecognizers,
     this.onPageStarted,
     this.onPageFinished,
-    this.onWebResourceError,
     this.debuggingEnabled = false,
     this.gestureNavigationEnabled = false,
     this.userAgent,
@@ -280,12 +276,6 @@ class WebView extends StatefulWidget {
   /// directly in the HTML has been loaded and code injected with
   /// [WebViewController.evaluateJavascript] can assume this.
   final PageFinishedCallback onPageFinished;
-
-  /// Invoked when a web resource has failed to load.
-  ///
-  /// This can be called for any resource (iframe, image, etc.), not just for
-  /// the main page.
-  final WebResourceErrorCallback onWebResourceError;
 
   /// Controls whether WebView debugging is enabled.
   ///
@@ -489,13 +479,6 @@ class _PlatformCallbacksHandler implements WebViewPlatformCallbacksHandler {
   void onPageFinished(String url) {
     if (_widget.onPageFinished != null) {
       _widget.onPageFinished(url);
-    }
-  }
-
-  @override
-  void onWebResourceError(WebResourceError error) {
-    if (_widget.onWebResourceError != null) {
-      _widget.onWebResourceError(error);
     }
   }
 
